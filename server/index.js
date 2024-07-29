6da2cf284fe855e6331f29037a1ae679393ae645
hassanalee <hassanali14563@gmail.com>
2024-07-29 10:26:47 +0500

    initialized prisma orm

--- server/index.js
+++ server/index.js
@@ -1,5 +1,30 @@
 const express = require("express");
 require("dotenv").config();
+const { PrismaClient } = require("@prisma/client");
+
 const app = express();
-const port = process.env.PORT;
-app.listen(port, () => console.log(`Server is listening on port ${port}...`));
+const port = process.env.PORT || 3000;
+const prisma = new PrismaClient();
+
+async function main() {
+  try {
+    // Connect to the PostgreSQL database
+    await prisma.$connect();
+    console.log("Connected to the database");
+
+    // Middleware to parse JSON bodies
+    app.use(express.json());
+
+    //  API endpoint
+
+    // Start the server
+    app.listen(port, () => {
+      console.log(`Server is listening on port ${port}...`);
+    });
+  } catch (error) {
+    console.error("Failed to connect to the database", error);
+    process.exit(1); // Exit the process with a failure code
+  }
+}
+
+main();
