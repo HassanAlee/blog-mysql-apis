fdcba7c56060b08ec0419249d46fcc5549bb292b
hassanalee <hassanali14563@gmail.com>
2024-07-29 09:15:07 +0500

    starting from scratch again

--- server/utils/password.js
+++
@@ -1,8 +0,0 @@
-const bcryptjs = require("bcryptjs");
-const generatePassword = (password) => {
-  return bcryptjs.hashSync(password, 10);
-};
-const checkPassword = (password, userPassword) => {
-  return bcryptjs.compareSync(userPassword, password);
-};
-module.exports = { generatePassword, checkPassword };
